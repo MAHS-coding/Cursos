fa1d384e509149057f6d4663db703371239469f8
MAHSCODING <mahscoding@gmail.com>
2025-05-18 18:01:28 -0400

    First Commit

--- src/main/java/com/Microservicio/Cursos/model/Curso.java
+++ src/main/java/com/Microservicio/Cursos/model/Curso.java
@@ -1,5 +1,15 @@
 package com.Microservicio.Cursos.model;
 
+import lombok.Data;
+
+
+
+@Data
 public class Curso {
     
+    private int idCurso;
+    private String tituloCurso;
+    private String descripcionCurso;
+    private int maxCupoCurso;
+    private int cuposDisponiblesCurso;
 }
