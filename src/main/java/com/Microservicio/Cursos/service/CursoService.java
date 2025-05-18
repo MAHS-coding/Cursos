77702f01a4c9c3fe3c177a912004b25cfb6834cd
MAHSCODING <mahscoding@gmail.com>
2025-05-18 19:09:30 -0400

    fixed the Service

--- src/main/java/com/Microservicio/Cursos/service/CursoService.java
+++ src/main/java/com/Microservicio/Cursos/service/CursoService.java
@@ -3,10 +3,12 @@ package com.Microservicio.Cursos.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.Microservicio.Cursos.model.Curso;
 import com.Microservicio.Cursos.repository.CursoRepository;
 
+@Service
 public class CursoService {
     @Autowired
     private CursoRepository cursoRepository;
